880ad7d801c90cfb3b2b0001025aa42a6d984cc9
luquinha <lucaspirlo55@gmail.com>
2020-07-18 19:14:53 -0300

    [feature]: add hover effect

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,9 @@ import colors from '../utils/colors'
 
 export default styled.button`
   appearance: none;
+  transition: all .325s ease-in-out;
   width: 100%;
+  cursor: pointer;
   text-align: center;
   background-color: ${colors.blue};
   border: none;
@@ -11,4 +13,8 @@ export default styled.button`
   padding: 20px 12px;
   color: white;
   font-size: 28px;
+
+  &:hover {
+    filter: brightness(1.75);
+  }
 `
