4639ba2ef60bdb70cf0eddc5ef885d3b3ca04fb6
luquinha <lucaspirlo55@gmail.com>
2020-07-18 19:03:51 -0300

    [feature]: add button component

---
+++ src/components/Button.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+import colors from '../utils/colors'
+
+export default styled.button`
+  appearance: none;
+  width: 100%;
+  text-align: center;
+  background-color: ${colors.blue};
+  border: none;
+  border-radius: 4px;
+  padding: 20px 12px;
+  color: white;
+  font-size: 28px;
+`
